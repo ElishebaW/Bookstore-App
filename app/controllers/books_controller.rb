d6a3e732916b6443a2e27f30cb7a7687f8eb9b08
ewigg <leighw33@gmail.com>
2017-03-12 21:40:41 +0000

    Add styling to index books page

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,12 @@
 class BooksController < ApplicationController
+   before_action :set_pin, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :correct_user, only: [:edit, :update, :destroy]
   def new
      @books = Book.new
   end
 
   def create
-   @books = current_user.books.build(book_params)
       if @book.save
         redirect_to @book, notice: 'Book was successfully created.' 
       else
@@ -30,3 +32,18 @@ class BooksController < ApplicationController
     @books = Book.find(params[:id])
   end
 end
+private
+  
+    def set_pin
+      @pin = Pin.find(params[:id])
+    end
+
+  
+    def pin_params
+      params.require(:books).permit(:description, :title)
+    end
+
+   def correct_user
+      @pin = current_user.pins.find_by(id: params[:id])
+      redirect_to pins_path, notice: "Not authorized to edit this plan" if @pin.nil?
+  end
